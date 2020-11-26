7f03f4519ae797dac57cb0554beb3c4d1d851dcb
Valentin <parlier.valentin@gmail.com>
2020-11-26 13:30:45 +0100

    Corrigé exo6

--- script.js
+++ script.js
@@ -1,165 +1,86 @@
+function reduireArray(array, size) {
+  if (array.length <= size) {
+    return [array];
+  }
+  return [array.slice(0, size), ...reduireArray(array.slice(size), size)];
+}
+
+const dateTimeFormat = Intl.DateTimeFormat("fr");
+
+function afficher(json){
+	const selections = reduireArray(json, 3);
+
+  let html = "";
+
+  selections.forEach(selection => {
+    html += '<div class="columns">';
+
+    selection.forEach((repo) => {
+      html += `
+            <div class="column">
+            <div class="card">
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img
+                    src="https://bulma.io/images/placeholders/1280x960.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div class="card-content">
+                <div class="media">
+                  <div class="media-left">
+                    <figure class="image is-48x48">
+                      <img
+                        src="https://bulma.io/images/placeholders/96x96.png"
+                        alt="Placeholder image"
+                      />
+                    </figure>
+                  </div>
+                  <div class="media-content">
+                    <p class="title is-4">${repo.name}</p>
+                    <p class="subtitle is-6">@Parcourir</p>
+                  </div>
+                </div>
+
+                <div class="content">
+                   ${repo.description}
+                  <br />
+                  Dernière mise à jour: <time datetime="${
+                    repo.updated_at
+                  }">${dateTimeFormat.format(new Date(repo.updated_at))}</time>
+                </div>
+              </div>
+            </div>
+          </div>`;
+    });
+    html += "</div>";
+  });
+
+  document.querySelector(".container").innerHTML = html;
+}
+
 document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://sharp-panini-f8b745.netlify.app//GalleryRepo/images.json")
-        .then(function (response) {
-            return response.json();
+  if (navigator.onLine) {
+    document.querySelector(".notification").setAttribute("hidden", "");
+  }
 
-        }).then(function (res) {
+  window.addEventListener("online", () => {
+    document.querySelector(".notification").setAttribute("hidden", "");
+  });
+  window.addEventListener("offline", () => {
+    document.querySelector(".notification").removeAttribute("hidden");
+  });
 
+  let fetchData;
+  if (navigator.onLine) {
+    fetchData = fetch("https://sharp-panini-f8b745.netlify.app/images.json")
+      .then((response) => response.json())
+      .then((data) => localforage.setItem("data", data));
+  }
+  else {
+    fetchData = localforage.getItem("data");
+  }
 
-
-
-
-
-            console.log(res);
-
-            let images = res;
-
-            let div_main = document.getElementById("main");
-
-            let container = document.createElement("div");
-            container.classList.add("container", "mt-5", "mb-3");
-
-            let row = document.createElement("div");
-            row.classList.add("row");
-
-            images.forEach(function (image) {
-                let div_image = document.createElement("div");
-                div_image.classList.add("col-sm-6", "col-md-4", "col-lg-3", "p-2");
-
-                let img = document.createElement("img");
-                img.src = image.src;
-                img.alt = image.alt;
-                img.title = image.title;
-                img.classList.add("w-100", "on_hover_clickable");
-
-                div_image.appendChild(img);
-                row.appendChild(div_image);
-            });
-
-            container.appendChild(row);
-            div_main.appendChild(container);
-
-        }).catch(function (err) {
-            console.log(err);
-        });
-        if (navigator.onLine) {
-            console.log("online");
-        } else {
-            console.log("offline");
-        }
-
-        window.addEventListener("online", function (e) {
-          console.log("change online");
-        });
-
-        window.addEventListener("offline", function (e) {
-          console.log("change offline");
-        });
+  fetchData.then((json) => afficher(json));
 });
-
-
-/*let div_main = document.getElementById("main");
-
-let container = document.createElement("div");
-container.classList.add("container", "mt-5", "mb-3");
-
-let row = document.createElement("div");
-row.classList.add("row");
-
-let images = [
-    {
-        src: "images/image1.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image2.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image3.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image4.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image5.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image6.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image7.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image8.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image9.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image10.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image11.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image12.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image13.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image14.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image15.jpg",
-        alt: "",
-        description: "",
-    },
-    {
-        src: "images/image16.jpg",
-        alt: "",
-        description: "",
-    },
-];
-
-images.forEach(function (image) {
-    let div_image = document.createElement("div");
-    div_image.classList.add("col-sm-6", "col-md-4", "col-lg-3", "p-2");
-
-    let img = document.createElement("img");
-    img.src = image.src;
-    img.classList.add("w-100", "on_hover_clickable");
-
-    div_image.appendChild(img);
-    row.appendChild(div_image);
-});
-
-container.appendChild(row);
-div_main.appendChild(container);*/
